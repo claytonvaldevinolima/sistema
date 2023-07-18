fe9921efe7a5d95b66f695ac2b06850ca123c0b5
clayton <asdflima@gmail.com>
2023-07-18 20:55:57 -0300

    terceiro commit

--- sistema_escolar.py
+++ sistema_escolar.py
@@ -4,4 +4,4 @@ nota_2 = int(input("digite uma nota"))
 nota_3 = int(input("digite uma nota"))
 
 media_final = (nota_1 + nota_2 + nota_3) / 3
-print(media_final)
+print("media final = ", media_final)
